047b702d8ede9d6a6770831f8d7ad560715c3fc4
Ahmad <62012204+apuyang@users.noreply.github.com>
2021-12-04 17:30:10 +0700

    Create aselole.js

---
+++ aselole.js
@@ -0,0 +1,13 @@
+<script>	
+//let attemps=0;
+const clearDsq = setInterval(() => {
+	//console.log(attemps++)
+	document.querySelectorAll('iframe[name^=dsq]').forEach(element => 	{
+		if (element.getAttribute('src') == null) { 
+			element.remove(); 
+			clearInterval(clearDsq);
+		}	
+	})
+}, 400);
+
+</script>	
